09627ebb90d0e4d3655eab88d6b8d7ebb1040525
AnupShrestha1 <anup.lubhoo@gmail.com>
2023-05-30 22:02:39 +0545

    made a simple interest calculator

---
+++ script.js
@@ -0,0 +1,11 @@
+function display(){
+    document.getElementById('name').innerHTML="anup";
+ }
+ function interest(){
+     let principal = document.getElementById('p').value;
+     let time = document.getElementById('t').value;
+     let rate = document.getElementById('r').value;
+     let si = (principal*rate*time)/100;
+     document.getElementById('interest').innerHTML=si;
+ }
+ 
